7558385cf0c2f9ec52a89f534ce1e2184b20de3b
Pedro Moura <107577096+plfmoura@users.noreply.github.com>
2022-11-03 00:54:13 -0300

    add(scripts): modal features/modal background

--- modal.js
+++ modal.js
@@ -1,16 +1,21 @@
 const modal = document.querySelector('.modal-container')
+const modalBg = document.querySelector('.modal-bg');
 
 function openModal() {
-  modal.classList.add('active')
+  modal.classList.add('active');
+  /*para tirar eventos de mouse atrás do modal enquanto tiver ativo*/
+  modalBg.classList.add('modal-bg-active');/*verificar no CSS funcionamento*/
 }
 
 function closeModal() {
-  modal.classList.remove('active')
+  modal.classList.remove('active');
+  modalBg.classList.remove('modal-bg-active');
 }
 
 const input = document.querySelector('#inputTxt');
 const btn = document.querySelector('#btn');
 const result = document.querySelector('.result');
+const spanTxt = document.querySelector('.result-modal');
 
 btn.addEventListener('click', () => {
   result.innerHTML = "";
@@ -19,45 +24,9 @@ btn.addEventListener('click', () => {
   
   if(text == 'teste'){
     openModal();
-    result.innerHTML = "";
-  
-  } else {
+    result.innerHTML = "";  /*retorno de resultado esperado \/ */
+    spanTxt.innerHTML = `O nome: <strong>${text.toUpperCase()}</strong> foi sorteado!`
+  } else {     /*retorno de resultado inesperado \/ */
     result.innerHTML = `Não encontrei ${text} nesta lista.`;
-  }
-
-})
-
-/* Desenvolver com switch
-
-btn.addEventListener('click', () => {
-  result.innerHTML = "";
-
-  var text = 1;
-  var nomes;
-  
-  switch(text){
-    case 0:
-      nomes == 2 
-      openModal();
-      break
-    case 1:
-      nomes == 4 
-      openModal();
-      break
-    case 2:
-      nomes == 5 
-      openModal();
-      break
-    case 3:
-      nomes == 6 
-      openModal();
-      break
-    case 4:
-      nomes == 1 
-      openModal();
-      break
-    default:
-      result.innerHTML = `Não encontrei ${text} nesta lista.`; 
-  }
-})
-*/
+  };
+});
