bba383eb7003480a5f5b1cec516116a64fb25f0b
Pedro Moura <107577096+plfmoura@users.noreply.github.com>
2022-11-04 00:11:48 -0300

    add(script): valida.js bug fixed/ reorder folder

---
+++ scripts/valida.js
@@ -0,0 +1,61 @@
+var assinantes = [
+    {
+      nome: "Roberto", 
+      inscricao: 121211
+    },
+  
+    {
+      nome: "Adriana", 
+      inscricao: 121212
+    },
+  
+    {
+      nome: "Valdeci", 
+      inscricao: 121214
+    },
+  
+    {
+      nome: "Pedro", 
+      inscricao: 121213
+    }
+];
+
+const modal = document.querySelector('.modal-container')
+const modalBg = document.querySelector('.modal-bg');
+
+function openModal() {
+  modal.classList.add('active');
+  /*para tirar eventos de mouse atrás do modal enquanto tiver ativo*/
+  modalBg.classList.add('modal-bg-active');/*verificar no CSS funcionamento*/
+}
+
+function closeModal() {
+  modal.classList.remove('active');
+  modalBg.classList.remove('modal-bg-active');
+}
+
+const input = document.querySelector('#inputTxt');
+const btn = document.querySelector('#btn');
+const result = document.querySelector('.result');
+const spanTxt = document.querySelector('.result-modal');
+
+btn.addEventListener('click', () => {
+  result.innerHTML = "";
+
+  var encontrarAssinante = assinantes.find((selecionado) => {
+    if(selecionado.inscricao == input.value){
+        var sorteado = selecionado.nome;
+    }
+
+    if(sorteado){
+        openModal();
+        result.innerHTML = "";  /*retorno de resultado esperado \/ */
+        spanTxt.innerHTML = `${sorteado} você foi sorteado!`
+    } else {     /*retorno de resultado inesperado \/ */
+        result.innerHTML = `Não encontrei ${input.value} nesta lista.`;
+    };
+      
+    return sorteado;
+});
+
+});
