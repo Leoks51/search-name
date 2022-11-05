b1a93c3e28558d7f0845a1045c253e347b3fbc18
Pedro Moura <107577096+plfmoura@users.noreply.github.com>
2022-11-04 22:07:59 -0300

    alte(script/css): show modal / layout program

--- scripts/valida.js
+++ scripts/valida.js
@@ -3,23 +3,25 @@ import { openModal } from "./modal.js";
 
 const input = document.querySelector('#inputTxt');
 const btn = document.querySelector('#btn');
-const result = document.querySelector('.result');
 const spanTxt = document.querySelector('.result-modal');
 
 btn.addEventListener('click', () => {
-  result.innerHTML = "";
-
+  
   var encontrarAssinante = assinantes.find((selecionado) => {
     if(selecionado.inscricao == input.value){
-        var sorteado = selecionado.nome;
+      var sorteado = selecionado.nome;
     }
-    console.log(encontrarAssinante);
+
+    var modalTitle = document.querySelector('.modal-title');
+
     if(sorteado){
         openModal();
-        result.innerHTML = "";  /*retorno de resultado esperado \/ */
-        spanTxt.innerHTML = `${sorteado} você foi sorteado!`
-    } else {     /*retorno de resultado inesperado \/ */
-        result.innerHTML = `Não encontrei ${input.value} nesta lista.`;
+        spanTxt.innerHTML = `O número <strong>${input.value}</strong> pertencente a <strong>${sorteado}</strong>, foi sorteado, parabéns!`
+        modalTitle.innerHTML = 'WOW!';
+    } else { 
+        openModal();
+        spanTxt.innerHTML = `Poxa, o número <strong>${input.value}</strong> não foi sorteado.`;
+        modalTitle.innerHTML = 'OOPS!';
     };
       
     return sorteado;
